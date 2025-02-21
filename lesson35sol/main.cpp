4699f95f141f7782bab3b018ac8e70d32b056d90
rasslabsya <artyomlordik@gmail.com>
2025-02-21 18:01:17 +0300

    avr markk

--- lesson35sol/main.cpp
+++ lesson35sol/main.cpp
@@ -8,7 +8,7 @@ int main() {
 	cout << "input ur marks: ";
 	cin >> mark1 >> mark2 >> mark3 >> mark4 >> mark5;
 
-	double avg = calculate_avr_mark( mark1, mark2, mark3, mark4, mark5);
+	double avg = calculate_avr_mark(mark1, mark2, mark3, mark4, mark5);
 
 	cout << "ur avr mark: " << avg << endl;
 
